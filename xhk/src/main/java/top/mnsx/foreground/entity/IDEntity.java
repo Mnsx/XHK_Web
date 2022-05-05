a318cfd49185f332c62489cc21d5e48b7a2d3edc
mnsx <xx1527030652@gmail.com>
2022-05-05 10:22:16 +0800

    1.5.5

--- xhk/src/main/java/top/mnsx/foreground/entity/IDEntity.java
+++
@@ -1,9 +0,0 @@
-package top.mnsx.foreground.entity;
-
-import lombok.Data;
-
-@Data
-public class IDEntity {
-    private Long userId;
-    private Long discountId;
-}
--- xhk/src/main/java/top/mnsx/foreground/service/KeyService.java
+++
@@ -1,7 +0,0 @@
-package top.mnsx.foreground.service;
-
-import javax.servlet.http.HttpSession;
-
-public interface KeyService {
-    Boolean useKey(String keyCode, Long curId);
-}
